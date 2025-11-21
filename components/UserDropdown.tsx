4ff7dd8eb600d7f1c1900536e5f9c66402da7797
Tycho <104978003+TychoBohm@users.noreply.github.com>
2025-11-21 15:50:55 +0100

    Update components/UserDropdown.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -42,7 +42,7 @@ const router = useRouter();
                 <div className="flex relative items-center gap-3 py-2">
                     <Avatar className="h-10 w-10">
                         <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback className="bg0yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
+                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
                     </Avatar>
                     <div className="flex flex-col">
                         <span className="text-base font-medium-text-gray-400">
